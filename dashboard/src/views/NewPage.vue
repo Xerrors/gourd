d2e8fed3832bd3aad2d9488fce87e6bf3173c375
Xerrors <xerrors@163.com>
2021-01-15 11:07:42 +0800

    temp

--- dashboard/src/views/NewPage.vue
+++ dashboard/src/views/NewPage.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="edit-container">
-    
+    <div class="edit-box">
+
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.edit-container {
+  width: 100%;
+  background: #e6e6e6;
+
+  .edit-box {
+    width: 1000px;
+    min-height: 500px;
+
+    margin: 40px auto;
+    background: white;
+    border: 1px solid #333;
+  }
+}
+</style>
